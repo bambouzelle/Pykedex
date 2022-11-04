5f0f9954e6754a94c9dac12b48ff7e3d23370c9e
MEUNIER Adrien <adrien.meunier42@gmail.com>
2022-11-04 13:52:38 +0100

    change path

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:pokemon_id>/', views.detail, name= 'detail')
+    path('pokedex/<int:pokemon_id>/', views.detail, name='detail')
 ]
--- pokemon/urls.py
+++ pokemon/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('pokedex/', include('pokedex.urls')),
+    path('', include('pokedex.urls')),
     path('admin/', admin.site.urls),
 ]
